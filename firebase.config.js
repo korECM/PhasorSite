734040cf3866cceed47a46de1eb108a157b42c4f
JongwonYoun <jeffyoun@naver.com>
2020-08-03 18:40:43 +0900

    FEAT: firebase config 추가

---
+++ firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ2GM7tXQOO2_5kb4wCeGg5JDCrZYhoe8",
+  authDomain: "phaser-beb24.firebaseapp.com",
+  databaseURL: "https://phaser-beb24.firebaseio.com",
+  projectId: "phaser-beb24",
+  storageBucket: "phaser-beb24.appspot.com",
+  messagingSenderId: "466733117172",
+  appId: "1:466733117172:web:34258ad90223b30c6b78a1",
+};
+
+export default firebaseConfig;
