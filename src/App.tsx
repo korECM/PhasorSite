56312d32fea07bd89f6ca5673031101ed8219f2f
JongwonYoun <jeffyoun@naver.com>
2020-08-03 18:56:49 +0900

    FEAT: firebase 기본 로딩 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import * as firebase from "firebase/app";
+import "firebase/database";
+
+import FirebaseConfig from "./firebase.config";
+
+export interface DataInterface {
+  latitude: string;
+  longitude: string;
+  magnitude: string;
+  time: string;
+}
 
 function App() {
+  const [data, setData] = useState<DataInterface[]>([]);
+
+  useEffect(() => {
+    firebase.initializeApp(FirebaseConfig);
+    const database = firebase.database();
+    const dbDataRef = database.ref("/roadDatas");
+
+    dbDataRef.on("value", (snapshot: firebase.database.DataSnapshot) => {
+      console.log(snapshot.val());
+      setData(Object.values(snapshot.val()) as any);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((e) => (
+        <div key={e.time}>
+          <div>위도 : {e.latitude}</div>
+          <div>경도 : {e.longitude}</div>
+          <div>심각도 : {e.magnitude}</div>
+          <div>시간 : {e.time}</div>
+        </div>
+      ))}
     </div>
   );
 }
